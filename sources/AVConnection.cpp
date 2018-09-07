913a7583342b745e818ee60edc89438dfa564ca6
SpyrosLahanas <lahanasspyros@hotmail.com>
2018-09-07 22:46:37 +0100

    Day's work 07/09/2018

--- sources/AVConnection.cpp
+++ sources/AVConnection.cpp
@@ -160,6 +160,47 @@ bool validate_interval(std::string& value)
     else return false;
 }
 
+bool validate_tech_interval(std::string& value)
+{
+    if(value == "1min" || value == "5min" || value == "15min" ||
+            value == "30min" || value == "60min" || value == "daily"
+            || value == "weekly" || value == "monthly") return true;
+    else return false;
+}
+
+bool validate_time_period(std::string& value)
+{
+    try
+    {
+        int arg = std::stoi(value);
+        return arg > 0;
+    }
+    catch(...)
+    {
+        return false;
+    }
+}
+
+bool validate_limit(std::string& value)
+{
+    try
+    {
+        double arg = std::stod(value);
+        return arg > 0;
+    }
+    catch(...)
+    {
+        return false;
+    }
+}
+
+bool validate_series_type(std::string& value)
+{
+    if(value == "close" || value == "open" || value == "high" || value ==
+            "low") return true;
+    else return false;
+}
+
 bool validate_outputsize(std::string& value)
 {
     if (value == "compact" || value == "full") return true;
@@ -364,4 +405,196 @@ APISpecs::APISpecs() : urlroot("https://www.alphavantage.co/query?")
 
     //ADD the function in specs
     api_specs[AVFunctions::FX_MONTHLY] = fx_monthly;
+
+    //Log the DIGITAL_CURRENCY_DAILY specs
+    //Construct the argument for market.
+    APIArgument market("market=", &always_true, false);
+
+    //Construct the function
+    APIFunction digital_daily;
+    digital_daily.funcName = "DIGITAL_CURRENCY_DAILY";
+    digital_daily.args.push_back(function);
+    digital_daily.args.push_back(symbol);
+    digital_daily.args.push_back(market);
+    digital_daily.args.push_back(apikey);
+
+    //ADD the function in specs
+    api_specs[AVFunctions::DIGITAL_CURRENCY_DAILY] = digital_daily;
+
+    //Log the DIGITAL_CURRENCY_WEEKLY specs
+    //Construct the function
+    APIFunction digital_weekly;
+    digital_weekly.funcName = "DIGITAL_CURRENCY_WEEKLY";
+    digital_weekly.args.push_back(function);
+    digital_weekly.args.push_back(symbol);
+    digital_weekly.args.push_back(market);
+    digital_weekly.args.push_back(apikey);
+
+    //ADD the function in specs
+    api_specs[AVFunctions::DIGITAL_CURRENCY_WEEKLY] = digital_weekly;
+
+    //Log the DIGITAL_CURRENCY_MONTHLY specs
+    //Construct the function
+    APIFunction digital_monthly;
+    digital_monthly.funcName = "DIGITAL_CURRENCY_MONTHLY";
+    digital_monthly.args.push_back(function);
+    digital_monthly.args.push_back(symbol);
+    digital_monthly.args.push_back(market);
+    digital_monthly.args.push_back(apikey);
+
+    //ADD the function in specs
+    api_specs[AVFunctions::DIGITAL_CURRENCY_MONTHLY] = digital_monthly;
+
+    //Log the SMA specs
+    //Construct the missing argument (i.e., interval, time_period and
+    //series_type).
+    APIArgument interval_tech("interval=", &validate_tech_interval, false);
+    APIArgument time_period("time_period=", &validate_time_period, false);
+    APIArgument series_type("series_type=", &validate_series_type, false);
+
+    //Construct the function
+    APIFunction sma;
+    sma.funcName = "SMA";
+    sma.args.push_back(function);
+    sma.args.push_back(symbol);
+    sma.args.push_back(interval_tech);
+    sma.args.push_back(time_period);
+    sma.args.push_back(series_type);
+    sma.args.push_back(datatype);
+    sma.args.push_back(apikey);
+
+    //ADD the function in specs
+    api_specs[AVFunctions::SMA] = sma;
+
+    //Log the EMA specs
+    //All arguments already constructed
+    //Construct the function
+    APIFunction ema;
+    ema.funcName = "EMA";
+    ema.args.push_back(function);
+    ema.args.push_back(symbol);
+    ema.args.push_back(interval_tech);
+    ema.args.push_back(time_period);
+    ema.args.push_back(series_type);
+    ema.args.push_back(datatype);
+    ema.args.push_back(apikey);
+
+    //ADD the function in specs
+    api_specs[AVFunctions::EMA] = ema;
+
+    //Log the WMA specs
+    //All arguments already constructed
+    //Construct the function
+    APIFunction wma;
+    wma.funcName = "WMA";
+    wma.args.push_back(function);
+    wma.args.push_back(symbol);
+    wma.args.push_back(interval_tech);
+    wma.args.push_back(time_period);
+    wma.args.push_back(series_type);
+    wma.args.push_back(datatype);
+    wma.args.push_back(apikey);
+
+    //ADD the function in specs
+    api_specs[AVFunctions::WMA] = wma;
+
+    //Log the DEMA specs
+    //All arguments already constructed
+    //Construct the function
+    APIFunction dema;
+    dema.funcName = "DEMA";
+    dema.args.push_back(function);
+    dema.args.push_back(symbol);
+    dema.args.push_back(interval_tech);
+    dema.args.push_back(time_period);
+    dema.args.push_back(series_type);
+    dema.args.push_back(datatype);
+    dema.args.push_back(apikey);
+
+    //ADD the function in specs
+    api_specs[AVFunctions::DEMA] = dema;
+
+    //Log the TEMA specs
+    //All arguments already constructed
+    //Construct the function
+    APIFunction tema;
+    tema.funcName = "TEMA";
+    tema.args.push_back(function);
+    tema.args.push_back(symbol);
+    tema.args.push_back(interval_tech);
+    tema.args.push_back(time_period);
+    tema.args.push_back(series_type);
+    tema.args.push_back(datatype);
+    tema.args.push_back(apikey);
+
+    //ADD the function in specs
+    api_specs[AVFunctions::TEMA] = tema;
+
+    //Log the TRIMA specs
+    //All arguments already constructed
+    //Construct the function
+    APIFunction trima;
+    trima.funcName = "TRIMA";
+    trima.args.push_back(function);
+    trima.args.push_back(symbol);
+    trima.args.push_back(interval_tech);
+    trima.args.push_back(time_period);
+    trima.args.push_back(series_type);
+    trima.args.push_back(datatype);
+    trima.args.push_back(apikey);
+
+    //ADD the function in specs
+    api_specs[AVFunctions::TRIMA] = trima;
+
+    //Log the KAMA specs
+    //All arguments already constructed
+    //Construct the function
+    APIFunction kama;
+    kama.funcName = "KAMA";
+    kama.args.push_back(function);
+    kama.args.push_back(symbol);
+    kama.args.push_back(interval_tech);
+    kama.args.push_back(time_period);
+    kama.args.push_back(series_type);
+    kama.args.push_back(datatype);
+    kama.args.push_back(apikey);
+
+    //ADD the function in specs
+    api_specs[AVFunctions::KAMA] = kama;
+
+    //Log the MAMA specs
+    //Construct missing arguments (i.e., fastlimi and slowlimit).
+    APIArgument fastlimit("fastlimit=", &validate_limit, true);
+    APIArgument slowlimit("slowlimit=", &validate_limit, true);
+
+    //Costruct the function
+    APIFunction mama;
+    mama.funcName = "MAMA";
+    mama.args.push_back(function);
+    mama.args.push_back(symbol);
+    mama.args.push_back(interval_tech);
+    mama.args.push_back(series_type);
+    mama.args.push_back(fastlimit);
+    mama.args.push_back(slowlimit);
+    mama.args.push_back(datatype);
+    mama.args.push_back(apikey);
+
+    //ADD the function in specs
+    api_specs[AVFunctions::MAMA] = mama;
+
+    //Log the T3 specs
+    //All arguments already constructed
+    //Construct the function
+    APIFunction t3;
+    t3.funcName = "T3";
+    t3.args.push_back(function);
+    t3.args.push_back(symbol);
+    t3.args.push_back(interval_tech);
+    t3.args.push_back(time_period);
+    t3.args.push_back(series_type);
+    t3.args.push_back(datatype);
+    t3.args.push_back(apikey);
+
+    //ADD the function in specs
+    api_specs[AVFunctions::T3] = t3;
 }
